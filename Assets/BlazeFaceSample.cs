5cf3a7d0e38df3f8f26d11c9bd9da122070e83d8
Adam <adam@stormhardt.com>
2023-08-19 00:09:03 +1000

    Add basic mobile friendly mechanics 1
    Camera listener
    - Abstract out Camera listener so we can manage state and call it in appropriate areas within the class
    - Add simple state for listener so we can track what we're doing with our camera
    - Remove Listener for camera when moving to background task. Re-add listener when re-focusing
    
    Mobile Sleep
    - Add sleep Timeout Neversleep setting on start because default app tends to go to sleep

--- Assets/BlazeFaceSample.cs
+++ Assets/BlazeFaceSample.cs
@@ -20,12 +20,20 @@ namespace NatML.Examples {
 
         private BlazeFacePredictor predictor;
 
+        private bool isListeningToCameraFrames = false;
+
         private async void Start () {
+
+            // Disable screen dimming for mobile devices
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
             // Create the RVM predictor
             predictor = await BlazeFacePredictor.Create();
             // Listen for camera frames
-            cameraManager.OnCameraFrame.AddListener(OnCameraFrame);
+            listenToCameraFrames();
         }
+
+        
 
         private void OnCameraFrame (CameraFrame frame) {
             // Predict
@@ -39,6 +47,33 @@ namespace NatML.Examples {
             cameraManager.OnCameraFrame.RemoveListener(OnCameraFrame);
             // Dispose the predictor
             predictor?.Dispose();
+        }
+
+        public void OnApplicationFocus(bool hasFocus) {
+            // Listen for camera frames when the app is in focus or moves to background
+            if (hasFocus) {
+                listenToCameraFrames();
+            } else {
+                stopListeningToCameraFrames();
+            }
+        }
+
+        // helper methods
+        private void listenToCameraFrames () {
+            // Listen for camera frames. Since this is a more common use case, we have a helper method for it.
+            if (isListeningToCameraFrames) return;
+
+            cameraManager.OnCameraFrame.AddListener(OnCameraFrame);
+            isListeningToCameraFrames = true;
+        }
+
+        private void stopListeningToCameraFrames () {
+            // Stop listening for camera frames
+
+            if (isListeningToCameraFrames) {
+                cameraManager.OnCameraFrame.RemoveListener(OnCameraFrame);
+                isListeningToCameraFrames = false;
+            }
         }
     }
 }
